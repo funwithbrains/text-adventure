1cb1c7b68a23bc0e475e4f830f1f34566cccf589
Cory <cory.andrew.1988@gmail.com>
2017-10-27 14:43:57 -0400

    removed unused duplicate code

--- assets/scripts/utils/collection.js
+++ assets/scripts/utils/collection.js
@@ -25,49 +25,7 @@ define(['./imports/index'], ({ _ }) => {
     };
   };
 
-  const sample = (rng, items) => {
-    return items[Math.floor(rng.quick() * items.length)];
-  };
-
-  const sampleRange = (rng, min, max) => min + Math.floor(rng.quick() * (max - min));
-
-  const createArraySource = (strings, order) => {
-    const beginnings = [];
-    const table = {};
-    strings.forEach(string => {
-      beginnings.push(string.slice(0, order));
-      const end = string.length - order;
-      for (let i = 0; i < end; ++i) {
-        const key = string.slice(i, i + order);
-        if (!table[key]) {
-          table[key] = [];
-        }
-        table[key].push(string[i + order]);
-      }
-    });
-
-    return {
-      strings,
-      order,
-      beginnings,
-      table,
-      create: (rng, minLength = 3, maxLength = 8) => {
-        const length = sampleRange(rng, minLength, maxLength);
-        let string = sample(rng, beginnings);
-        while (string.length < length) {
-          const items = table[string.slice(-order)];
-          if (!items) { break; }
-
-          string += sample(rng, items);
-        }
-
-        return string;
-      }
-    };
-  };
-
   return {
-    createWeightedSampler,
-    createArraySource
+    createWeightedSampler
   };
 });
