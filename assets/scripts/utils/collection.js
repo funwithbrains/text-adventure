4e854c4924d2d04e8e5fd7a010df46fb9d5cf0e1
Cory <cory.andrew.1988@gmail.com>
2017-10-31 17:46:42 -0400

    added weighted sampler bucket

--- assets/scripts/utils/collection.js
+++ assets/scripts/utils/collection.js
@@ -1,13 +1,22 @@
 define(['./imports/index'], ({ _ }) => {
-  const createWeightedSampler = (items) => {
-    if (items.length === 0) {
-      throw new Error('A sampler cannot be created from an empty array.');
-    }
+  // TODO consider using tree structures if we ever need faster weighted sampling
 
+  const computeTotalWeight = (items) => {
     let totalWeight = 0;
+
     for (let i = 0; i < items.length; ++i) {
       totalWeight += items[i].weight;
     }
+
+    return totalWeight;
+  };
+
+  const createWeightedSampler = (items) => {
+    if (items.length === 0) {
+      return { sample: () => null };
+    }
+
+    const totalWeight = computeTotalWeight(items);
 
     return {
       sample: (rng) => {
@@ -20,12 +29,41 @@ define(['./imports/index'], ({ _ }) => {
           }
         }
 
-        throw new Error('The weights are inconsistent.');
+        return null;
+      }
+    };
+  };
+
+  // "bucket" is meant to indicate that each item is removed when it is retrieved
+  const createWeightedSamplerBucket = (items) => {
+    if (items.length === 0) {
+      return { sample: () => null };
+    }
+
+    items = items.slice(); // clone items because we're going to mutate it
+
+    let totalWeight = computeTotalWeight(items);
+
+    return {
+      sample: (rng) => {
+        const weightedPosition = rng.double() * totalWeight;
+        let passedWeight = 0;
+        for (let i = 0; i < items.length; ++i) {
+          passedWeight += items[i].weight;
+          if (passedWeight > weightedPosition) {
+            const result = items.splice(i, 1)[0];
+            totalWeight -= result.weight;
+            return result;
+          }
+        }
+
+        return null;
       }
     };
   };
 
   return {
-    createWeightedSampler
+    createWeightedSampler,
+    createWeightedSamplerBucket
   };
 });
