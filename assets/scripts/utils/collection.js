3de6ea60a337b4aa99a2e57fe27d996272f27c59
Cory <cory.andrew.1988@gmail.com>
2017-11-03 16:26:39 -0400

    added lazy table to collection util

--- assets/scripts/utils/collection.js
+++ assets/scripts/utils/collection.js
@@ -1,5 +1,4 @@
 define(['./imports/index'], ({ _ }) => {
-
   const mapRange = (start, end, f) => {
     const result = [];
 
@@ -10,7 +9,23 @@ define(['./imports/index'], ({ _ }) => {
     return result;
   };
 
+  const createLazyTable = (f) => {
+    const table = {};
+
+    return {
+      get: (x, y) => {
+        const key = x + '|' + y;
+        if (!table[key]) {
+          table[key] = f(x, y);
+        }
+
+        return table[key];
+      }
+    };
+  };
+
   return {
-    mapRange
+    mapRange,
+    createLazyTable
   };
 });
